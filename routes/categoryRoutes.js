d950eddd2f4e6d36f4a9f3bb5deb34b3000bd4ef
priyanka <priyankaji856@gmail.com>
2025-04-28 11:34:40 +0530

    Initial commit

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const upload = require('../middleware/multer'); 
-// const upload = require('../middleware/uploads');
+const upload = require('../middleware/multer'); // Import multer upload middleware
 const { categoryInsert, categoryGet, categoryEdit, categoryDelete } = require("../controllers/CategoryController");
 
 const router = express.Router();
 
-// Route
+// Route to insert a new category with image upload
 router.post('/insertcategory', upload.single('image'), categoryInsert);
+
+// Route to get all categories
 router.get('/getcategory', categoryGet);
+
+// Route to edit an existing category by ID with image upload
 router.put('/editcategory/:id', upload.single('image'), categoryEdit);
+
+// Route to delete a category by ID
 router.delete('/deletecategory/:id', categoryDelete);
 
 module.exports = router;
