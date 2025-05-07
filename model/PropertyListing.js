fbf466d87d69d2acbe6f3f11859722e679858dca
priyanka <priyankaji856@gmail.com>
2025-05-07 13:31:58 +0530

    category

---
+++ model/PropertyListing.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const propertylisting = new mongoose.Schema({
+    title:{
+        type:String,
+        require : true
+    },
+    subtitle:{
+        type:String,
+        require: true
+    },
+    description:{
+        type:String,
+        require : true
+    },
+    buttontitle:{
+        type:String
+    },
+    image:{
+       type:Buffer
+    },
+    imageType: {
+        type : String 
+    },
+    data:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+const propertylistingdata = mongoose.model('propertylisting',propertylisting);
+module.exports = propertylistingdata;
---
+++ model/SubtosubCategory.js
@@ -0,0 +1,20 @@
+// models/Sub Category.js
+const mongoose = require('mongoose');
+
+const subtoSubCategorySchema = new mongoose.Schema({
+    masterId :{
+        type:String
+    },
+    subcategoryId:{
+        type:String
+    },
+    categorytype: { type: String, required: true },
+    categoryvalue: { type: String, required: true },
+    image: { type: Buffer },
+    imageType: {
+        type : String 
+    },
+    action: { type: String }
+}, { timestamps: true });
+
+module.exports = mongoose.model('SubtoSubCategory', subtoSubCategorySchema);
