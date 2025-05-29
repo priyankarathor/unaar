317805f86f5a00a37446db289d67e124c24773d2
priyanka <priyankaji856@gmail.com>
2025-05-29 13:17:11 +0530

    all

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer(); // Memory storage, handles text fields
+
 const locationController = require('../controllers/locationController');
 
-router.post('/locationinsert', locationController.locationInsert);
+router.post('/locationinsert', upload.none(), locationController.locationInsert); // <-- Important
 router.get('/locationall', locationController.locationsGet);
 
 module.exports = router;
