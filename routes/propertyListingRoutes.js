048ca77931ccba442bf25ab516eda04c5a18db25
priyanka <priyankaji856@gmail.com>
2025-05-30 12:57:12 +0530

    all

--- routes/propertyListingRoutes.js
+++ routes/propertyListingRoutes.js
@@ -5,18 +5,7 @@ const propertylistingController = require('../controllers/PropertyListingControl
 
 const storage = multer.memoryStorage();
 
-const upload = multer({
-  storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
-  fileFilter: (req, file, cb) => {
-    if (['image/jpeg', 'image/png', 'image/webp'].includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error('Only JPEG, PNG, or WEBP images are allowed.'));
-    }
-  }
-});
-
+const upload = multer({ storage: storage });
 // CSV Import
 // router.post('/import-csv', upload.single('file'), propertylistingController.importCSV);
 
