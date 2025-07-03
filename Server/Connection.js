788ccf76a897543d84787cc40b2f1f429706663b
priyanka rathore <priyankaji856@gmail.com>
2025-07-03 12:58:36 +0530

    new Update

--- Server/Connection.js
+++ Server/Connection.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 
+const uri = 'mongodb+srv://unaarNewProject:A1g8CJ3Cj0PfYQGy@unaarproject01.sug5zje.mongodb.net/?retryWrites=true&w=majority&appName=unaarProject01';
 
-const uri = 'mongodb+srv://unaarproject:VTf!3_idEHBnGV2@unaarproject01.sug5zje.mongodb.net/?retryWrites=true&w=majority&appName=unaarProject01';
 const connectDB = async () => {
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB Atlas!'))
-.catch((err) => console.error('Connection error:', err));
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ Connected to MongoDB Atlas!');
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
