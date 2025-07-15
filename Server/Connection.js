3b66fbcba2820d8add1e00cca6895bed5e934511
priyankarathor <priyankaji856@gmail.com>
2025-07-15 18:45:47 +0530

    all image link

--- Server/Connection.js
+++ Server/Connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const uri = 'mongodb+srv://unaarproject:VTf!3_idEHBnGV2@unaarproject01.sug5zje.mongodb.net/?retryWrites=true&w=majority&appName=unaarProject01';
+const uri = 'mongodb+srv://unaarproject:3fCw44WjwkfZkdSa@unaarproject01.sug5zje.mongodb.net/?retryWrites=true&w=majority&appName=unaarProject01';
 const connectDB = async () => {
 mongoose.connect(uri, {
   useNewUrlParser: true,
