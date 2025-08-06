1a29cc35b926a7611e58ba180b26cbecb63f10fe
priyankarathor <priyankaji856@gmail.com>
2025-08-06 12:42:19 +0530

    new update

--- middleware/multer.js
+++ middleware/multer.js
@@ -8,7 +8,7 @@ const s3 = new AWS.S3();
 const upload = multer({
   storage: multerS3({
     s3,
-    bucket: "getstarting-s3-unaar-demostration",
+    bucket: "getstarting-s3-unaar-demostration01",
     acl: 'public-read',
     metadata: (req, file, cb) => {
       cb(null, { fieldName: file.fieldname });
