3ac514724a21a6afc02511abfb104f80812fdbed
priyankarathor <priyankaji856@gmail.com>
2025-08-07 11:10:51 +0530

    aws update

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,3 +1,4 @@
+// awsMulter.js
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
@@ -6,9 +7,9 @@ require('dotenv').config();
 const s3 = new AWS.S3({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
   },
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_REGION
 });
 
 const upload = multer({
@@ -19,9 +20,9 @@ const upload = multer({
       cb(null, { fieldName: file.fieldname });
     },
     key: function (req, file, cb) {
-      cb(null, `${Date.now()}_${file.originalname}`);
-    },
-  }),
+      cb(null, Date.now() + '-' + file.originalname);
+    }
+  })
 });
 
 module.exports = upload;
