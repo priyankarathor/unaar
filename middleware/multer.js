821acf6e73d59576508e62f12315f94bdafca4c5
priyankarathor <priyankaji856@gmail.com>
2025-08-12 22:26:21 +0530

    aws

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,12 +1,12 @@
+// multerConfig.js
 require("dotenv").config();
 const multer = require("multer");
 
-// Store file in memory (not on disk) for manual S3 upload in controller
-const storage = multer.memoryStorage();
+const storage = multer.memoryStorage(); // Store files in memory for AWS or DB upload
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // limit file size to 5MB
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB max
   fileFilter: (req, file, cb) => {
     const allowedTypes = ["image/jpeg", "image/png", "image/jpg", "image/webp"];
     if (!allowedTypes.includes(file.mimetype)) {
