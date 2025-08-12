12168becc307a95bf020c0ac463c49264d335739
priyankarathor <priyankaji856@gmail.com>
2025-08-12 10:42:53 +0530

    all

--- routes/agencieRoutes.js
+++ routes/agencieRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const storage = multer.memoryStorage();
-const upload = require("../middleware/multer");
+const upload = multer({ storage: storage });
 const { agenciesadd, agenciesget, agenciesedit, agenciesdelete } = require("../controllers/agencieController");
 
 // Define your routes
